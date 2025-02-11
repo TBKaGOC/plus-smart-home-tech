e175182097f03edea1d5a685ada529557ad9ffc4
GOfC <guardianofchaos6@gmail.com>
2025-02-11 19:01:15 +0300

    Реализовать функционал

--- telemetry/collector/src/main/java/ru/gofc/smart_home/EventController.java
+++ telemetry/collector/src/main/java/ru/gofc/smart_home/EventController.java
@@ -2,12 +2,14 @@ package ru.gofc.smart_home;
 
 import jakarta.validation.Valid;
 import lombok.AccessLevel;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.gofc.smart_home.hub.exception.HubMapException;
+import ru.gofc.smart_home.hub.kafka.HubProducer;
 import ru.gofc.smart_home.hub.model.HubEvent;
 import ru.gofc.smart_home.sensor.exception.SensorMapException;
 import ru.gofc.smart_home.sensor.model.SensorEvent;
@@ -15,18 +17,19 @@ import ru.gofc.smart_home.sensor.kafka.SensorProducer;
 
 @RestController
 @RequestMapping("/events")
-@RequiredArgsConstructor
+@AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class EventController {
-    SensorProducer producer;
+    SensorProducer sensorProducer;
+    HubProducer hubProducer;
 
     @PostMapping("/sensors")
     public void postSensorEvent(@Valid @RequestBody SensorEvent event) throws SensorMapException {
-        producer.sendMessage(event);
+        sensorProducer.sendMessage(event);
     }
 
     @PostMapping("/hub")
-    public void postHubEvent(@Valid @RequestBody HubEvent event) {
-
+    public void postHubEvent(@Valid @RequestBody HubEvent event) throws HubMapException {
+        hubProducer.sendMessage(event);
     }
 }
