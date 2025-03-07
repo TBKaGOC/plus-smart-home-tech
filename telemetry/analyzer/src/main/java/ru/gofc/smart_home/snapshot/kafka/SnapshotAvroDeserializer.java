754578e4a4511be0c0cbb7a6a3cf6f8e662035ea
GOfC <guardianofchaos6@gmail.com>
2025-03-07 18:28:06 +0300

    Исправить SnapshotAvroDeserializer.java

--- telemetry/analyzer/src/main/java/ru/gofc/smart_home/snapshot/kafka/SnapshotAvroDeserializer.java
+++ telemetry/analyzer/src/main/java/ru/gofc/smart_home/snapshot/kafka/SnapshotAvroDeserializer.java
@@ -5,6 +5,10 @@ import ru.gofc.smart_home.hub.kafka.BaseAvroDeserializer;
 import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
 
 public class SnapshotAvroDeserializer extends BaseAvroDeserializer<SensorsSnapshotAvro> {
+    public SnapshotAvroDeserializer() {
+        super(SensorsSnapshotAvro.getClassSchema());
+    }
+
     public SnapshotAvroDeserializer(Schema schema) {
         super(schema);
     }
