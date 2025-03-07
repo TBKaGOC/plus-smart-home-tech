2e76cb75a4956255a7f15a57ea940857d130edc0
GOfC <guardianofchaos6@gmail.com>
2025-03-07 18:24:36 +0300

    Исправить HubEventDeserializer.java

--- telemetry/analyzer/src/main/java/ru/gofc/smart_home/hub/kafka/HubEventDeserializer.java
+++ telemetry/analyzer/src/main/java/ru/gofc/smart_home/hub/kafka/HubEventDeserializer.java
@@ -4,6 +4,10 @@ import org.apache.avro.Schema;
 import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
 
 public class HubEventDeserializer extends BaseAvroDeserializer<HubEventAvro> {
+    public HubEventDeserializer() {
+        super(HubEventAvro.getClassSchema());
+    }
+
     public HubEventDeserializer(Schema schema) {
         super(schema);
     }
