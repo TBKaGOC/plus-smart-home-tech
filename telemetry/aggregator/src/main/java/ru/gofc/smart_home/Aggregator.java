8b2ac7dd0fa89a594638c629f58aa2b63e7b9b69
GOfC <guardianofchaos6@gmail.com>
2025-03-14 16:16:04 +0300

    Исправить aggregator

--- telemetry/aggregator/src/main/java/ru/gofc/smart_home/Aggregator.java
+++ telemetry/aggregator/src/main/java/ru/gofc/smart_home/Aggregator.java
@@ -1,24 +1,18 @@
 package ru.gofc.smart_home;
 
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import ru.gofc.smart_home.snapshot.AggregatorStarter;
 
 @SpringBootApplication
 public class Aggregator {
+    @Autowired
     private static AggregatorStarter starter;
 
     public static void main(String[] args) {
         SpringApplication.run(Aggregator.class, args);
 
         starter.start();
-    }
-
-    @Autowired
-    public static void setStarter(AggregatorStarter aggregatorStarter) {
-        starter = aggregatorStarter;
     }
 }
