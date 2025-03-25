ba5e3f24e43518ca8b9bb62169e77310f5738b17
GOfC <guardianofchaos6@gmail.com>
2025-03-25 21:33:39 +0300

    Реализовать DiscoveryServer.java

--- telemetry/aggregator/src/main/java/ru/gofc/smart_home/Aggregator.java
+++ telemetry/aggregator/src/main/java/ru/gofc/smart_home/Aggregator.java
@@ -3,11 +3,13 @@ package ru.gofc.smart_home;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 import ru.gofc.smart_home.snapshot.AggregatorStarter;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableDiscoveryClient
 public class Aggregator {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Aggregator.class, args);
--- telemetry/collector/src/main/java/ru/gofc/smart_home/EventsServer.java
+++ telemetry/collector/src/main/java/ru/gofc/smart_home/EventsServer.java
@@ -3,9 +3,11 @@ package ru.gofc.smart_home;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableDiscoveryClient
 public class EventsServer {
     public static void main(String[] args) {
         SpringApplication.run(EventsServer.class, args);
